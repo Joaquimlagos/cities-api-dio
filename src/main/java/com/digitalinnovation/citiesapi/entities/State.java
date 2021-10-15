660f603aa9ccb0c108c662461c057d74cb06a4b0
Joaquim Lagos <joaquim.lagos2000@gmail.com>
2021-10-15 17:07:41 -0300

    criação das entidades coutry e state

---
+++ src/main/java/com/digitalinnovation/citiesapi/entities/State.java
@@ -0,0 +1,46 @@
+package com.digitalinnovation.citiesapi.entities;
+
+import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
+import java.util.List;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+
+import lombok.Data;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+
+@Entity(name = "State")
+@Table(name = "estado")
+@Data
+@TypeDefs({
+        @TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
+})
+public class State {
+
+  @Id
+  private Long id;
+
+  @Column(name = "nome")
+  private String name;
+
+  private String uf;
+
+  private Integer ibge;
+
+  @ManyToOne
+  @JoinColumn(name = "pais", referencedColumnName = "id")
+  private Country country;
+
+  @Type(type = "jsonb")
+  @Basic(fetch = FetchType.LAZY)
+  @Column(name = "ddd", columnDefinition = "jsonb")
+  private List<Integer> ddd;
+
+  }
