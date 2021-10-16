f8e998a6c26c9997eb3d2add0b4cba2422190bee
Joaquim Lagos <joaquim.lagos2000@gmail.com>
2021-10-16 15:44:33 -0300

    criação da classe cities/distance

--- src/main/java/com/digitalinnovation/citiesapi/controllers/CitiesController.java
+++ src/main/java/com/digitalinnovation/citiesapi/controllers/CitiesController.java
@@ -4,19 +4,19 @@ package com.digitalinnovation.citiesapi.controllers;
 import java.util.List;
 import java.util.Optional;
 
+import com.digitalinnovation.citiesapi.entities.City;
 import com.digitalinnovation.citiesapi.entities.Country;
 import com.digitalinnovation.citiesapi.entities.State;
+import com.digitalinnovation.citiesapi.repositories.CityRepository;
 import com.digitalinnovation.citiesapi.repositories.CountryRepository;
 import com.digitalinnovation.citiesapi.repositories.StateRepository;
+import com.digitalinnovation.citiesapi.services.DistanceService;
 import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/")
@@ -25,9 +25,11 @@ public class CitiesController {
 
   private final CountryRepository repositoryCountry;
   private final StateRepository repositoryState;
+  private final CityRepository repositoryCity;
+  private final DistanceService service;
 
   @GetMapping("/countries")
-  public Page<Country> cities(Pageable page) {
+  public Page<Country> countries(Pageable page) {
     return repositoryCountry.findAll(page);
   }
 
@@ -44,5 +46,21 @@ public class CitiesController {
   @GetMapping("/states")
   public List<State> staties() {
     return repositoryState.findAll();
+  }
+
+  @GetMapping("/cities")
+  public Page<City> cities(final Pageable page) {
+    return repositoryCity.findAll(page);
+  }
+
+  @GetMapping("/by-points")
+  public ResponseEntity byPoints(@RequestParam(name = "from") final Long city1,
+                         @RequestParam(name = "to") final Long city2) {
+    return ResponseEntity.ok().body(service.distanceByPointsInMiles(city1, city2));
+  }
+  @GetMapping("/by-cube")
+  public ResponseEntity byCube(@RequestParam(name = "from") final Long city1,
+                       @RequestParam(name = "to") final Long city2) {
+    return ResponseEntity.ok().body(service.distanceByCubeInMeters(city1, city2));
   }
 }
---
+++ src/main/java/com/digitalinnovation/citiesapi/entities/City.java
@@ -0,0 +1,40 @@
+package com.digitalinnovation.citiesapi.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import com.digitalinnovation.citiesapi.utils.PointType;
+import lombok.Data;
+import org.hibernate.annotations.Type;
+import org.hibernate.annotations.TypeDef;
+import org.hibernate.annotations.TypeDefs;
+import org.springframework.data.geo.Point;
+
+
+@Entity
+@Table(name = "cidade")
+@Data
+@TypeDefs(value = {
+        @TypeDef(name = "point", typeClass = PointType.class)
+})
+public class City {
+
+  @Id
+  private Long id;
+
+  @Column(name = "nome")
+  private String name;
+
+  private Integer uf;
+
+  private Integer ibge;
+
+  @Column(name = "lat_lon")
+  private String geolocation;
+
+  @Type(type = "point")
+  @Column(name = "lat_lon", updatable = false, insertable = false)
+  private Point location;
+  }
